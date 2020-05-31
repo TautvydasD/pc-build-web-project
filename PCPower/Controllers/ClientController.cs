bfbe37f5f9198f9e390b02bf4d85db574dbfec57
tautvydas.diksas <tautvydas.diksas@ktu.edu>
2020-05-31 09:22:05 +0300

    added view order list Crud

--- PCPower/Controllers/ClientController.cs
+++ PCPower/Controllers/ClientController.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using PCPower.Models;
 
 namespace PCPower.Controllers
 {
@@ -10,6 +13,8 @@ namespace PCPower.Controllers
     {
         //return View() grazina viewsa toki koks funkcijos pavadinimas ty. 
         //func openRepair(){return View()} grazins openRepair() viewsa
+
+        private PCPowerEntities db = new PCPowerEntities();
 
         // GET: Client
         public ActionResult Index()
@@ -82,10 +87,84 @@ namespace PCPower.Controllers
         {
             return View();
         }
-        public ActionResult selectUpdateOrder()
+
+        //
+        // GET: /Client/selectUpdateOrder/2
+        public ActionResult selectUpdateOrder(int? id)
         {
-            return View();
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Order order = db.Orders.Find(id);
+            if (order == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.fk_Client_Id = new SelectList(db.Clients, "Id", "User.Username", order.fk_Client_Id);
+            ViewBag.fk_Shop_Id = new SelectList(db.Shops, "Id", "Name", order.fk_Shop_Id);
+            ViewBag.fk_Receipt_Id = new SelectList(db.Receipts, "Id", "Id", order.fk_Receipt_Id);
+            return View("OrderForm", order);
         }
+
+        //
+        // POST: /Client/selectUpdateOrder/2
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult selectUpdateOrder([Bind(Include = "Id,fk_Shop_Id,fk_Receipt_Id,fk_Client_Id")] Order order)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(order).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("openOrders");
+            }
+            ViewBag.fk_Client_Id = new SelectList(db.Clients, "Id", "User.Username", order.fk_Client_Id);
+            ViewBag.fk_Shop_Id = new SelectList(db.Shops, "Id", "Name", order.fk_Shop_Id);
+            ViewBag.fk_Receipt_Id = new SelectList(db.Receipts, "Id", "Id", order.fk_Receipt_Id);
+            return View(order);
+        }
+
+
+        //
+        // GET: /Client/selectDeleteOrder/5
+        public ActionResult selectDeleteOrder(FormCollection fcNotUsed, int id = 0)
+        {
+            Order order = db.Orders.Find(id);
+            if (order == null)
+            {
+                return HttpNotFound();
+            }
+            db.Orders.Remove(order);
+            db.SaveChanges();
+            return RedirectToAction("openOrders");
+        }
+        //public ActionResult selectDeleteOrder(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    Order order = db.Orders.Find(id);
+        //    if (order == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(order);
+        //}
+
+        ////
+        //// POST: /Client/selectDeleteOrder/5
+        //[HttpPost, ActionName("selectDeleteOrder")]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult checkAction(int id)
+        //{
+        //    Order order = db.Orders.Find(id);
+        //    //db.Orders.Remove(order);
+        //    db.SaveChanges();
+        //    return RedirectToAction("Index");
+        //}
+
         public ActionResult dataUpdateSubmit()
         {
             return View();
@@ -94,19 +173,19 @@ namespace PCPower.Controllers
         {
             return View();
         }
+
+        //
+        // GET: /Client/openOrders/
+
         public ActionResult openOrders()
         {
-            return View("OrderList");
+            return View("OrderList", db.Orders.ToList());
         }
         public ActionResult orderSelection()
         {
             return View();
         }
         public ActionResult invokeOperation()
-        {
-            return View();
-        }
-        public ActionResult checkAction()
         {
             return View();
         }
